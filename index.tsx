7590627e38089e22d8c43554fc8808e715939a30
rizwan-zl <rizwan.zkazi@ziroh.com>
2023-05-02 18:16:48 +0530

    Latest update

--- index.tsx
+++ index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BadgeComponent } from './badge/badge.component'
 
-export type BadgeType = "PRIMARY" | "SECONDARY";
+// export type BadgeType = "PRIMARY" | "SECONDARY";
 
-type BadgeProps = {
-    count: number,
-    badgeType: BadgeType
-}
+// type BadgeProps = {
+//     count: number,
+//     badgeType: BadgeType
+// }
 
-export const Index = (props: BadgeProps) => {
-    const count = props.count;
-    const badgeType = props.badgeType;
-    return (
-        // <BadgeComponent count={10} badgeType='PRIMARY' />
-        <BadgeComponent count={count} badgeType={badgeType} />
+// export const Index = (props: BadgeProps) => {
+//     const count = props.count;
+//     const badgeType = props.badgeType;
+//     return (
+//         // <BadgeComponent count={10} badgeType='PRIMARY' />
+//         <BadgeComponent count={count} badgeType={badgeType} />
+//     );
+// }
 
-    );
+export {
+    BadgeComponent
 }
