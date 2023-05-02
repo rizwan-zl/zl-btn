2515c2374ad80cf43d40b327722f65240d8be428
rizwan-zl <rizwan.zkazi@ziroh.com>
2023-05-02 18:18:58 +0530

    Removed commented part

--- index.tsx
+++ index.tsx
@@ -1,22 +1,6 @@
 import React from 'react';
 import { BadgeComponent } from './badge/badge.component'
 
-// export type BadgeType = "PRIMARY" | "SECONDARY";
-
-// type BadgeProps = {
-//     count: number,
-//     badgeType: BadgeType
-// }
-
-// export const Index = (props: BadgeProps) => {
-//     const count = props.count;
-//     const badgeType = props.badgeType;
-//     return (
-//         // <BadgeComponent count={10} badgeType='PRIMARY' />
-//         <BadgeComponent count={count} badgeType={badgeType} />
-//     );
-// }
-
 export {
     BadgeComponent
 }
