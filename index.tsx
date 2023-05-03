8a4b806661a4432754e7275203d001a4e748460f
rizwan-zl <rizwan.zkazi@ziroh.com>
2023-05-03 10:20:37 +0530

    .

--- index.tsx
+++ index.tsx
@@ -1,5 +1,3 @@
 import React from 'react';
-// import { BadgeComponent } from './badge/badge.component'
-// export { BadgeComponent };
-
-export { default as BadgeComponent } from './badge/badge.component';
+import BadgeComponent from './badge/badge.component'
+export { BadgeComponent };
